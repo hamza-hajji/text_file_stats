5d87cf976f5f01de12a619244ded5d2c48ddb680
Hamza Hajji <hmzhjji@gmail.com>
2017-04-23 22:37:38 +0100

    Add logic for multiple files through command-line arguments

--- analyzer.rb
+++ analyzer.rb
@@ -1,20 +1,22 @@
-FILE_NAME = "oliver.txt"
+file_names = ARGV
 
-lines = File.readlines FILE_NAME
-text = lines.join
-line_count = lines.size
-character_count = text.length
-character_count_no_space = text.gsub(/\s+/, "").length
-word_count = text.split.length
-sentence_count = text.split(/\.|\?|!/).length
-paragraph_count = text.split(/\n\n/).length
+file_names.each do |file_name|
+  lines = File.readlines file_name
+  text = lines.join
+  line_count = lines.size
+  character_count = text.length
+  character_count_no_space = text.gsub(/\s+/, "").length
+  word_count = text.split.length
+  sentence_count = text.split(/\.|\?|!/).length
+  paragraph_count = text.split(/\n\n/).length
 
-puts "#{FILE_NAME.upcase}"
-puts " *#{line_count} lines."
-puts " *#{character_count} characters."
-puts " *#{character_count_no_space} characters(no whitespace)."
-puts " *#{word_count} words."
-puts " *#{sentence_count} sentences."
-puts " *#{paragraph_count} paragraphs."
-puts " *#{sentence_count / paragraph_count} sentence per paragraph."
-puts " *#{word_count / sentence_count} word per sentence."
+  puts "#{file_name.upcase}"
+  puts " *#{line_count} lines."
+  puts " *#{character_count} characters."
+  puts " *#{character_count_no_space} characters(no whitespace)."
+  puts " *#{word_count} words."
+  puts " *#{sentence_count} sentences."
+  puts " *#{paragraph_count} paragraphs."
+  puts " *#{sentence_count / paragraph_count} sentence per paragraph."
+  puts " *#{word_count / sentence_count} word per sentence."
+end
